048d179f391f78d70f5d719c6b9fc3d664f1a632
Robert F. Nash <robert@robertnash.net>
2013-08-27 18:14:44 -0400

    elixir: bob: refactored Teenage.hey to make meaning of conditions apparent (as sug. by drueck

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,9 +1,18 @@
 defmodule Teenager do
+  defp isShouting(heard) do
+    String.upcase(heard) == heard
+  end
+  defp isQuestion(heard) do
+    String.ends_with? heard, "?"
+  end
+  defp isSilence(heard) do
+    String.lstrip(heard) == ""
+  end
   def hey(heard) do
     cond do
-      String.lstrip(heard) == "" -> "Fine. Be that way!"
-      String.ends_with? heard, "?" -> "Sure."
-      String.upcase(heard) == heard -> "Woah, chill out!"
+      isSilence(heard) -> "Fine. Be that way!"
+      isQuestion(heard) -> "Sure."
+      isShouting(heard) -> "Woah, chill out!"
       true -> "Whatever."
     end
   end
