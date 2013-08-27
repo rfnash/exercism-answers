157565b88d13d9b94fe919279ec2fa87699327a3
Robert F. Nash <robert@robertnash.net>
2013-08-27 16:22:22 -0400

    elixir: bob: fixed horrible formatting

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,10 @@
 defmodule Teenager do
   def hey(a) do
-		cond do
-			String.lstrip(a) == "" ->
-        "Fine. Be that way!"
-      String.ends_with? a, "?" ->
-				"Sure."
-      String.upcase(a) == a ->
-         "Woah, chill out!"
-			true ->
-				"Whatever."
-		end
+    cond do
+      String.lstrip(a) == "" -> "Fine. Be that way!"
+      String.ends_with? a, "?" -> "Sure."
+      String.upcase(a) == a -> "Woah, chill out!"
+      true -> "Whatever."
+    end
   end
 end
