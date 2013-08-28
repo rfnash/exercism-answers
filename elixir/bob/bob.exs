db7052560accbe8a605f01598d3bb94dfd1d857f
Robert F. Nash <robert@robertnash.net>
2013-08-27 20:42:49 -0400

    elixir: bob: changed method names to fit elixir conventions

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,18 +1,18 @@
 defmodule Teenager do
-  defp isShouting(heard) do
+  defp shouting?(heard) do
     String.upcase(heard) == heard
   end
-  defp isQuestion(heard) do
+  defp question?(heard) do
     String.ends_with? heard, "?"
   end
-  defp isSilence(heard) do
+  defp silence?(heard) do
     String.lstrip(heard) == ""
   end
   def hey(heard) do
     cond do
-      isSilence(heard) -> "Fine. Be that way!"
-      isQuestion(heard) -> "Sure."
-      isShouting(heard) -> "Woah, chill out!"
+      silence?(heard) -> "Fine. Be that way!"
+      question?(heard) -> "Sure."
+      shouting?(heard) -> "Woah, chill out!"
       true -> "Whatever."
     end
   end
